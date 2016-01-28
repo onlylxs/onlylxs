22535db9dde6d8824609dd3e3b0cf3c21312dd42
onlylxs <872482259@qq.com>
2016-01-28 15:33:30 +0800

    Lxs

---
+++ js/center.js
@@ -0,0 +1,8 @@
+var bannerSwiper = new Swiper('#banner-swiper', {
+    autoplay: 5000,
+    speed: 500,
+    loop: true,
+    prevButton: '.swiper-button-prev',
+    nextButton: '.swiper-button-next',
+    pagination: '.swiper-pagination'
+})
