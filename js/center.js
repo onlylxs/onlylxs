3babbfaa30da3a53804867a43fb8b06781ac1b55
onlylxs <872482259@qq.com>
2016-02-03 10:59:15 +0800

    Lxs

--- js/center.js
+++ js/center.js
@@ -6,3 +6,23 @@ var bannerSwiper = new Swiper('#banner-swiper', {
     nextButton: '.swiper-button-next',
     pagination: '.swiper-pagination'
 });
+
+$(function () {
+    var grid1=$(".content-grid");
+    var grid2=$(".articleList");
+    var grid3=$(".semi-background");
+    var grid4=$(".gallery-grid");
+    var grid5=$(".content-grid2");
+    var grid1Top=grid1.offset().top;
+    var grid2Top=grid2.offset().top;
+    var grid3Top=grid3.offset().top;
+    var grid4Top=grid4.offset().top;
+    var grid5Top=grid5.offset().top;
+    window.onscroll = function () {
+        var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+        console.log(scrollTop-grid1.height()+"--"+grid1Top)
+        if(scrollTop+grid1.height()>grid1Top){
+            grid1.find(".boxInfo").addClass("selected");
+        }
+    }
+});
