91f903fd90aba8463bb6f1b94f59f9049bff5e9e
onlylxs <872482259@qq.com>
2016-02-18 10:09:00 +0800

    Lxs

--- js/center.js
+++ js/center.js
@@ -8,11 +8,11 @@ var bannerSwiper = new Swiper('#banner-swiper', {
 });
 
 $(function () {
-    var grid1=$(".content-grid");
-    var grid2=$(".articleList");
-    var grid3=$(".semi-background");
-    var grid4=$(".gallery-grid");
-    var grid5=$(".content-grid2");
+    var grid1=$(".content-grid1");
+    var grid2=$(".content-grid2");
+    var grid3=$(".content-grid3");
+    var grid4=$(".content-grid4");
+    var grid5=$(".content-grid5");
     var grid1Top=grid1.offset().top;
     var grid2Top=grid2.offset().top;
     var grid3Top=grid3.offset().top;
@@ -20,17 +20,20 @@ $(function () {
     var grid5Top=grid5.offset().top;
     window.onscroll = function () {
         var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-        if(scrollTop+300>grid1Top){
+        if(scrollTop+grid1.height()>grid1Top){
             grid1.find(".boxInfo").addClass("selected");
         }
-        if(scrollTop+300>grid2Top){
+        if(scrollTop+grid2.height()>grid2Top){
             grid2.find(".col-md-4,.panel-tabContainer").addClass("selected");
         }
-        if(scrollTop+300>grid3Top){
+        if(scrollTop+grid3.height()>grid3Top){
             grid3.find(".col-lg-6").addClass("selected");
         }
-        if(scrollTop+300>grid4Top){
+        if(scrollTop+grid4.height()>grid4Top){
             grid4.find(".bottom-grid").addClass("selected");
+        }
+        if(scrollTop+grid5.height()>grid5Top){
+            grid5.addClass("selected");
         }
     };
 });
